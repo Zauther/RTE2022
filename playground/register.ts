5a9b9f960efe1b2f0b5d02f29d3d4780191b47ed
zhaozong.zzz <zhaozong.zzz@alibaba-inc.com>
2022-08-10 23:48:17 +0800

    debug

--- playground/register.ts
+++ playground/register.ts
@@ -38,17 +38,17 @@ apps.push({
     fastboard.manager.addApp({
       kind: BuiltinApps.DocsViewer,
       options: {
-        scenePath: annualrings,
+        scenePath: 'annualrings',
         title: "docs1", // 可选
         scenes: [
           {
             name: "2",
             ppt: {
               height: 1010,
-              src: 'https://scripts-res.oss-cn-shanghai.aliyuncs.com/%E5%88%98%E4%BC%AF%E9%92%8A.pdf?Expires=1660176957&OSSAccessKeyId=TMP.3Kg7SEu5jyCHkVELcVXJMJqpjUCtEwec6u2iSYmoWrNXKxHbJbNoCheHuTYZ9yDDkZPbc4DRRCZNjmbZpeyJWdcSQS3Mg3&Signature=emelSTgrBuv2b1xS2ahsXFaE1PQ%3D',
+              // src: 'https://scripts-res.oss-cn-shanghai.aliyuncs.com/%E5%88%98%E4%BC%AF%E9%92%8A.pdf?Expires=1660176957&OSSAccessKeyId=TMP.3Kg7SEu5jyCHkVELcVXJMJqpjUCtEwec6u2iSYmoWrNXKxHbJbNoCheHuTYZ9yDDkZPbc4DRRCZNjmbZpeyJWdcSQS3Mg3&Signature=emelSTgrBuv2b1xS2ahsXFaE1PQ%3D',
               // src: "https://convertcdn.netless.link/staticConvert/18140800fe8a11eb8cb787b1c376634e/1.png",
               // src: "https://cwiki.cn/downloads/%E5%B9%B4%E8%BD%AE/%E5%B0%81%E9%9D%A2.PDF",
-              // src: 'https://cwiki.cn/downloads/%E5%B9%B4%E8%BD%AE/%E5%89%A7%E6%9C%AC/%E5%88%98%E4%BC%AF%E9%92%8A.PDF',
+              src: 'https://cwiki.cn/downloads/%E5%B9%B4%E8%BD%AE/%E5%89%A7%E6%9C%AC/%E5%88%98%E4%BC%AF%E9%92%8A.PDF',
               // src: "https://scripts-res.oss-cn-shanghai.aliyuncs.com/%E5%B0%81%E9%9D%A2.pdf?Expires=1660175887&OSSAccessKeyId=TMP.3Kg7SEu5jyCHkVELcVXJMJqpjUCtEwec6u2iSYmoWrNXKxHbJbNoCheHuTYZ9yDDkZPbc4DRRCZNjmbZpeyJWdcSQS3Mg3&Signature=6w1CYrLdEo9RcqthYFiNoPe5%2FhU%3D",
               width: 714,
             }},
@@ -69,18 +69,18 @@ apps.push({
   },
 });
 
-apps.push({
-  kind: ScriptsShow.kind,
-  label: ScriptsShow.kind.replace(/([a-z])([A-Z])/g, "$1 $2"),
-  icon: annualrings,
-  onClick: (fastboard) => {
-    fastboard.manager.addApp({
-      kind: Dice,
-      options: {
-        scenePath: "/scripts/annualrings.jpg",
-        title: "docs1", // 可选
-        scenes: [], // SceneDefinition[] 静态/动态 Scene 数据
-      },
-    });
-  },
-});
+// apps.push({
+//   kind: ScriptsShow.kind,
+//   label: ScriptsShow.kind.replace(/([a-z])([A-Z])/g, "$1 $2"),
+//   icon: annualrings,
+//   onClick: (fastboard) => {
+//     fastboard.manager.addApp({
+//       kind: Dice,
+//       options: {
+//         scenePath: "/scripts/annualrings.jpg",
+//         title: "docs1", // 可选
+//         scenes: [], // SceneDefinition[] 静态/动态 Scene 数据
+//       },
+//     });
+//   },
+// });
