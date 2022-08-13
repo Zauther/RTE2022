7e7522038b1a99e8567208ed035f96572dd83bbb
zhaozong.zzz <zhaozong.zzz@alibaba-inc.com>
2022-08-13 16:54:30 +0800

    token

--- src/module/apps.ts
+++ src/module/apps.ts
@@ -18,17 +18,17 @@ export const registering = () => {
             label: "剧本",
             icon: "../assets/play.png",
             onClick: (fastboard: any) => {
-                // fastboard.manager.addApp({
-                //     kind: "Play",
-                //     // options: { 
-                //     //   scenePath: `/ppt/${1234567}`,
-                //     //   title: "剧本" 
-                //     // },
-                //     // attributes: {
-                //     //   taskId: "1234567", // [2]
-                //     //   url: "https://cwiki.cn/downloads/%E5%B9%B4%E8%BD%AE/%E5%89%A7%E6%9C%AC/%E5%88%98%E4%BC%AF%E9%92%8A.PDF", // [3]
-                //     // },
-                // });
+                fastboard.manager.addApp({
+                    kind: "Play",
+                    // options: { 
+                    //   scenePath: `/ppt/${1234567}`,
+                    //   title: "剧本" 
+                    // },
+                    // attributes: {
+                    //   taskId: "1234567", // [2]
+                    //   url: "https://cwiki.cn/downloads/%E5%B9%B4%E8%BD%AE/%E5%89%A7%E6%9C%AC/%E5%88%98%E4%BC%AF%E9%92%8A.PDF", // [3]
+                    // },
+                });
             },
         }
     );
