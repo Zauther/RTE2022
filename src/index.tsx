e8e9cf64d6f064d56abe1e773ff87bb5e8511207
zsh199808 <zsh199808@alibaba-inc.com>
2022-08-14 22:59:44 +0800

    feat: voice list

--- src/index.tsx
+++ src/index.tsx
@@ -1,68 +1,40 @@
-import React, { useCallback, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import './index.less';
 import { registering } from "./module/apps";
 import { get_uid, search_parse } from "./utils/common";
+import { Role, newRoom, joinRoom } from "./utils/NetApi";
 
 import Roles from "./module/roles";
 import Toolbar from "./module/toolbar";
 import Window from "./module/window";
 import Board from "./module/board";
 
-import { Role, newRoom, joinRoom } from "./utils/NetApi";
-import useMethods from "./state/State";
-
-
-
 function App() {
   registering();
-<<<<<<< HEAD
   const [show, setShow] = useState<boolean>(false);
   const [options, setOptions] = useState<{uuid: string, roomToken:string} | null>(null);
-=======
-  const [show, setShow] = useState(false);
-  const {toggleWindow} = useMethods({
-    toggleWindow(){
-      setShow(!show);
-    }
-    // console.log("toggleWindow inner0======= ", show)
-    // setShow(!show);
-    // console.log("toggleWindow inner1======= ", show)
-  });
 
-  // const toggleWindow = useCallback(() => {
-  //   console.log("toggleWindow inner0======= ", show)
-  //   setShow(!show)
-  //   console.log("toggleWindow inner1======= ", show)
-  // }, [show]);
-
-
-
-  
-  console.log("show ======= ", show)
-  const [options, setOptions] = useState<{uuid: string, roomToken:string} | null>(null);
-  
->>>>>>> bdef2c5849277a984f4335825a3b2adcf0e8b7fd
-
-  // 如果没有room_token则生成一个作为房间
-  if (!/uuid/.test(window.location.search)) {
-    // newRoom(Role.Admin).then((res: any)=>{
-    //   // window.alert(`已创建房间，可通过链接邀请玩家加入：${window.location.href + "&uuid=" + res.uuid}`);
-    //   setOptions({
-    //     uuid: res.uuid,
-    //     roomToken: res.roomToken
-    //   });
-    // });
-  } else {
-    const search_obj = search_parse();
-    const uuid = search_obj['uuid'];
-    joinRoom(Role.Writer, uuid).then((res: any) => {
-      setOptions({
-        uuid: res.uuid,
-        roomToken: res.roomToken
+  useEffect(() => {
+    if (!/uuid/.test(window.location.search)) { // 如果没有uuid则生成一个作为房间
+      newRoom(Role.Admin).then((res: any)=>{
+        // window.alert(`已创建房间，可通过链接邀请玩家加入：${window.location.href + "&uuid=" + res.uuid}`);
+        setOptions({
+          uuid: res.uuid,
+          roomToken: res.roomToken
+        });
       });
-    });
-  }
+    } else { // 如果有uuid则加入uuid对应的房间
+      const search_obj = search_parse();
+      const uuid = search_obj['uuid'];
+      joinRoom(Role.Writer, uuid).then((res: any) => {
+        setOptions({
+          uuid: res.uuid,
+          roomToken: res.roomToken
+        });
+      });
+    }
+  }, [])
 
   return (
     <div className="app">
