86072940fe60d3c39968b23cba7c1380e851e453
zhaozong.zzz <zhaozong.zzz@alibaba-inc.com>
2022-08-14 14:38:27 +0800

    rtc client & token

---
+++ src/utils/RTCUtils.ts
@@ -0,0 +1,80 @@
+import AgoraRTC, { IAgoraRTCClient, IAgoraRTCRemoteUser, IMicrophoneAudioTrack, UID } from "agora-rtc-sdk-ng";
+
+
+
+/*
+ * 使用姿势
+
+ */
+// 一年有效token
+// 00616cca950aca74708a9c3f1e2b7f2e655IACiT5BFZ5x40VfaOuPY/QfsIrz8exoggXVcj9DO9cto2t15FHwAAAAAIgCLKlkRauj5YgQAAQDqMtNkAgDqMtNkAwDqMtNkBADqMtNk
+
+// const client = getRtcClient();
+//     const option: Options = {
+//         appid: "16cca950aca74708a9c3f1e2b7f2e655",
+//         channel: "rte2022",
+//         uid: "0",
+//         token: "00616cca950aca74708a9c3f1e2b7f2e655IADoC/hZXCMWpUeaWKarZWI6K0gxQc4GQGFuYz4Nl7YdP915FHwAAAAAIgA6Jek7Gtz5YgQAAQCqmPhiAgCqmPhiAwCqmPhiBACqmPhi"
+//     }
+//     join(client, option).then((info) => {
+//         console.log(`===client ===${JSON.stringify(info)}`);
+//     });
+
+// 一年有效期，起始时间：20220814 14:30
+export const RTC_TOKEN = "00616cca950aca74708a9c3f1e2b7f2e655IACiT5BFZ5x40VfaOuPY/QfsIrz8exoggXVcj9DO9cto2t15FHwAAAAAIgCLKlkRauj5YgQAAQDqMtNkAgDqMtNkAwDqMtNkBADqMtNk";
+
+export interface Options {
+    appid: string,
+    channel: string,
+    uid: UID,
+    token: string
+}
+
+export interface RTCInfo {
+    options: Options;
+    audioTrack?: IMicrophoneAudioTrack;
+    user?: IAgoraRTCRemoteUser;
+    mediaType?: "audio" | "video"
+
+}
+
+export function getRtcClient(): IAgoraRTCClient {
+    return AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
+}
+
+export async function join(client: IAgoraRTCClient, options: Options): Promise<RTCInfo> {
+    client.on("user-published", async (user, mediaType) => {
+        // Subscribe to the remote user when the SDK triggers the "user-published" event
+        client.subscribe(user, mediaType);
+        console.log("subscribe success");
+
+        // If the remote user publishes an audio track.
+        if (mediaType === "audio") {
+            // Get the RemoteAudioTrack object in the AgoraRTCRemoteUser object.
+            const remoteAudioTrack = user.audioTrack;
+            // Play the remote audio track.
+            remoteAudioTrack && remoteAudioTrack.play();
+        }
+    });
+    client.on("user-unpublished", async (user) => {
+        // Unsubscribe from the tracks of the remote user.
+        client.unsubscribe(user);
+    });
+    var rtcInfo: RTCInfo = {
+        options: options,
+    };
+
+    [options.uid, rtcInfo.audioTrack] = await Promise.all([
+        // join the channel
+        client.join(options.appid, options.channel, options.token || null),
+        // create local tracks, using microphone and camera
+        AgoraRTC.createMicrophoneAudioTrack(),
+        // AgoraRTC.createCameraVideoTrack()
+    ]);
+
+    await client.publish([rtcInfo.audioTrack]);
+    console.log("publish success");
+    return new Promise<RTCInfo>((resolve, reject) => {
+        resolve(rtcInfo);
+    })
+}
